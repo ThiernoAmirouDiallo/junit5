b548be577abc201fde6180ce0041715e770593de
tdiallo <thiernoamiroud@gmail.com>
2019-11-03 14:35:58 -0500

    repeating a test multiple times with RepeatedTest

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -55,6 +56,7 @@ public class StringTest {
     }
 
     @Test
+    @RepeatedTest(10)
     void assertBooleans() {
         assertFalse("abcdefgh".contains("amir"));
     }
