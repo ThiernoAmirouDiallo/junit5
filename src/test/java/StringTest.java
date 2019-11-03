da52186ff47bdb3949d6b89bebc0c00becd61303
tdiallo <thiernoamiroud@gmail.com>
2019-11-03 13:53:45 -0500

    AssertThrows, BeforAll and AfterAll

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -1,18 +1,32 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 
 public class StringTest {
 
-    @BeforeEach //Befor in JUnit 4
+    @BeforeAll
+    static void beforAll() {
+        System.out.println("Initializing DB connetion");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("Closing DB connetion");
+    }
+
+    @BeforeEach
+        //Befor in JUnit 4
     void beforEach(TestInfo info) {
         System.out.println("Initializing test data for : " + info.getDisplayName());
     }
 
-    @AfterEach //After in JUnit4
+    @AfterEach
+        //After in JUnit4
     void AfterEach(TestInfo info) {
         System.out.println("Cleaning test data for : " + info.getDisplayName());
     }
@@ -48,6 +62,15 @@ public class StringTest {
         String[] expectedResult = {"montreal", "quebec", "ottawa"};
 
         assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void lengthException() {
+        String str = null;
+
+        assertThrows(NullPointerException.class, () -> {
+            str.length();
+        });
     }
 
 }
